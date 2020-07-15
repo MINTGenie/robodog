ec611213fd712cc8c86bea47d31df14ea0e12a73
MINT Genie <66748747+MINTGenie@users.noreply.github.com>
2020-07-15 03:10:07 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,30 +1,51 @@
-makerbit.connectUltrasonicDistanceSensor(DigitalPin.P2, DigitalPin.P1)
-music.playMelody("D C - D C - - - ", 347)
+robotbit.MotorStopAll()
+music.setVolume(10)
+music.playTone(587, music.beat(BeatFraction.Half))
+music.rest(music.beat(BeatFraction.Half))
+music.playTone(523, music.beat(BeatFraction.Quarter))
 let cntr = 0
 cntr = 50
+let SPEED = 500
 let left = true
+makerbit.connectUltrasonicDistanceSensor(DigitalPin.P2, DigitalPin.P1)
+basic.showNumber(3)
+basic.pause(500)
+basic.showNumber(2)
+basic.pause(200)
+basic.showNumber(1)
+basic.pause(100)
+basic.showIcon(IconNames.Happy)
 basic.forever(function () {
     if (makerbit.isUltrasonicDistanceLessThan(10, DistanceUnit.CM)) {
         robotbit.MotorRunDual(
         robotbit.Motors.M1A,
-        160,
+        SPEED,
         robotbit.Motors.M2B,
-        160
+        SPEED
         )
         basic.pause(200)
-        robotbit.MotorRunDual(
-        robotbit.Motors.M1A,
-        160,
-        robotbit.Motors.M2B,
-        -160
-        )
+        if (left) {
+            robotbit.MotorRunDual(
+            robotbit.Motors.M1A,
+            SPEED,
+            robotbit.Motors.M2B,
+            0 - SPEED
+            )
+        } else {
+            robotbit.MotorRunDual(
+            robotbit.Motors.M1A,
+            0 - SPEED,
+            robotbit.Motors.M2B,
+            SPEED
+            )
+        }
         basic.pause(200)
     } else {
         robotbit.MotorRunDual(
         robotbit.Motors.M1A,
-        -160,
+        0 - SPEED,
         robotbit.Motors.M2B,
-        -160
+        0 - SPEED
         )
     }
     if (left) {
